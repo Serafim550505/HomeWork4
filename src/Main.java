ac66dc0e311006e133c494031d2928948bec6d83
Serafim550505 <serafimdurekon6317@gmail.com>
2026-05-28 05:53:48 +0300

    HomWork 4 улучшенный

--- src/Main.java
+++ src/Main.java
@@ -6,84 +6,85 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println(" домашнее задание № 4 ");
+        System.out.println(" домашнее задание № 4 \n");
 
-        System.out.println(" Задача № 1  ");
+        System.out.println(" Задача № 1 ");
         int year = 17;
         if (year >= 18) {
-            System.out.printf("человеку 18 или больше лет");
+            System.out.println("человеку 18 или больше лет\n");
         }
         if (year < 18) {
-            System.out.printf("возраст совершеннолетия еще не наступил,нужно немного подождать  ");
+            System.out.println("возраст совершеннолетия еще не наступил,нужно немного подождать \n ");
         }
 
 
-        System.out.printf("Задача №2 ");
+        System.out.println("Задача №2 ");
         int temperature = 5;
         if (temperature < 5) {
-            System.out.printf("На улице холодно, нужно надеть шапку");
+            System.out.println("На улице холодно, нужно надеть шапку\n");
         } else {
-            System.out.printf("Сегодня тепло, можно идти без шапки");
+            System.out.println("Сегодня тепло, можно идти без шапки\n");
         }
 
 
-        System.out.printf(" Задача №3  ");
+        System.out.println(" Задача №3  ");
 
         int speed = 80;
         if (speed > 60) {
-            System.out.printf("скорость превышена");
+            System.out.println("скорость превышена\n");
         } else {
-            System.out.printf("превышения скорости нет");
+            System.out.println("превышения скорости нет  \n");
+
         }
 
 
-        System.out.printf("  Задача № 4 ");
+        System.out.println("  Задача № 4 ");
 
         int age = 20;
         if (age > 2 && age <= 6) {
-            System.out.printf("нужно ходить в детский сад  ");
+            System.out.println("нужно ходить в детский сад\n  ");
         } else if (age >= 7 && age <= 17) {
-            System.out.printf(" нужно ходить в школу ");
+            System.out.println(" нужно ходить в школу \n");
         } else if (age >= 18 && age <= 24) {
-            System.out.printf(" Нужно ходить в университет ");
+            System.out.println(" Нужно ходить в университет \n");
         }
         else  {
-            System.out.printf(" Надо ходить на работу ");
+            System.out.println(" Надо ходить на работу\n ");
         }
 
 
-        System.out.printf(" Задача №5 ");
+        System.out.println(" Задача №5 ");
         if (age <= 5) {
-            System.out.printf(" Нельзя кататься на аттракционе  ");
+            System.out.println(" Нельзя кататься на аттракционе  \n");
         }
         else if (age > 5 && age <= 14) {
-            System.out.printf(" можно  кататься только в сопровождении взрослого  ");
+            System.out.println(" можно  кататься только в сопровождении взрослого \n ");
         }
         else  {
-            System.out.printf("  можно кататься без сопровождения взрослого  ");
+            System.out.println("  можно кататься без сопровождения взрослого \n ");
         }
 
 
         System.out.println(" задача №6 ");
         int Capacity = 100;
         if (Capacity < 60) {
-            System.out.printf(" В вагоне есть сидячие места ");
+            System.out.println(" В вагоне есть сидячие места \n");
         } else if (Capacity > 102) {
-            System.out.printf(" В вагоне мест нет ");
+            System.out.println(" В вагоне мест нет \n");
         } else {
-            System.out.printf(" В вагоне есть стоячие места ");
+            System.out.println(" В вагоне есть стоячие места \n");
         }
 
-        System.out.printf(" Задача № 7 ");
+        System.out.println(" Задача № 7 ");
         int one = 9665;
         int two = 8068;
         int three = 7885;
         if (one >= two && one >= three) {
-            System.out.printf(" Самое большое число  " + one);
+            System.out.println(" Самое большое число \n " + one);
         } else if (two >= one && two >= three) {
-            System.out.printf(" Самое большое число  " + two);
+            System.out.println(" Самое большое число \n " + two);
         } else {
-            System.out.printf(" Самое большое число " + three);
+            System.out.println(" Самое большое число \n" + three);
         }
 
 
